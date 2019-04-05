9531b3139c5a86150a67eaf4662adea0e1e6faec
Mateusz <mateusz.lachowski@stxnext.pl>
2019-04-05 20:00:39 +0200

    Added name field for CreatePlanItem

--- src/screens/createPlanItem/CreatePlanItemScreen.tsx
+++ src/screens/createPlanItem/CreatePlanItemScreen.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { NavigationInjectedProps } from 'react-navigation';
 
-import { FullScreenTemplate } from 'components';
+import { Card, FullScreenTemplate } from 'components';
+import { PlanItemHeader } from "./PlanItemHeader";
+import { PlanItem } from "../../models";
 
 export class CreatePlanItemScreen extends React.PureComponent<
   NavigationInjectedProps
 > {
   render() {
-    return <FullScreenTemplate />;
+    const plan = this.props.navigation.getParam('plan');
+    const planItem = new PlanItem;
+    return (
+        <FullScreenTemplate padded darkBackground>
+          <Card>
+            <PlanItemHeader planItem={planItem} />
+          </Card>
+        </FullScreenTemplate>
+    );
   }
 }
