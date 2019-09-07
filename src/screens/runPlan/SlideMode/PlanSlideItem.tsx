ac1c026573641e0be93246b81be92b251f5bef43
Marek Sladczyk <marek.sladczyk@stxnext.pl>
2019-09-07 14:59:06 +0200

    31 - display image or image and text in slide

--- src/screens/runPlan/SlideMode/PlanSlideItem.tsx
+++ src/screens/runPlan/SlideMode/PlanSlideItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
-import { PlanItem } from 'models';
+import { PlanItem, StudentDisplayOption } from 'models';
 import { palette } from 'styles';
 import { PlanItemName } from '../PlanItemName';
 import { PlanItemTimer } from '../PlanItemTimer';
@@ -11,19 +11,50 @@ interface Props {
   index: number;
   textSize: string;
   textCase: string;
+  type: StudentDisplayOption;
 }
 
 export class PlanSlideItem extends React.PureComponent<Props> {
-  render() {
+  get showText(): boolean {
+    const { type } = this.props;
     return (
-      <View style={styles.container} >
-        <PlanItemName
-          planItemName={this.props.planItem.name}
-          textCase={this.props.textCase}
-          textSize={this.props.textSize}
-          textColor={styles.nameTextColor}
-        />
-          {(!!this.props.planItem.time) ? <PlanItemTimer itemTime={this.props.planItem.time} /> : null}
+      type === StudentDisplayOption.ImageWithTextSlide ||
+      type === StudentDisplayOption.TextSlide
+    );
+  }
+
+  get showImage(): boolean {
+    const { type } = this.props;
+    return (
+      type === StudentDisplayOption.ImageWithTextSlide ||
+      type === StudentDisplayOption.LargeImageSlide
+    );
+  }
+
+  render() {
+    const { planItem } = this.props;
+    return (
+      <View style={styles.container}>
+        {this.showImage && (
+          <View style={styles.imageContainer}>
+            <Image
+              resizeMode="contain"
+              style={styles.image}
+              source={{ uri: `data:image/jpeg;base64,${planItem.image}` }}
+            />
+          </View>
+        )}
+        {this.showText && (
+          <PlanItemName
+            planItemName={this.props.planItem.name}
+            textCase={this.props.textCase}
+            textSize={this.props.textSize}
+            textColor={styles.nameTextColor}
+          />
+        )}
+        {!!this.props.planItem.time ? (
+          <PlanItemTimer itemTime={this.props.planItem.time} />
+        ) : null}
       </View>
     );
   }
@@ -31,11 +62,19 @@ export class PlanSlideItem extends React.PureComponent<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: palette.background,
-    flexDirection: 'row',
-    alignItems: 'center',
     flex: 1,
-    margin: 0,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: palette.background,
+  },
+  imageContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    marginBottom: 16,
+  },
+  image: {
+    flex: 1,
   },
   nameTextColor: {
     color: palette.textBlack,
