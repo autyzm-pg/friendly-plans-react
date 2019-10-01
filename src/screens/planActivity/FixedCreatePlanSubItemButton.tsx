0adbe401d8361a7fb58af509651a46e6f94a4b3b
Kinga Wigurska-Bąk <kinga.wigurskabak@wipro.com>
2019-10-01 15:42:48 +0200

    CR fixes

--- src/screens/planActivity/FixedCreatePlanSubItemButton.tsx
+++ src/screens/planActivity/FixedCreatePlanSubItemButton.tsx
@@ -1,62 +1,88 @@
 import React from 'react';
 import { FloatingAction } from 'react-native-floating-action';
 
-import { Icon, StyledText } from 'components';
+import { Icon } from 'components';
 import { i18n } from 'locale';
-import { palette } from 'styles';
+import { fonts, palette } from 'styles';
 
 const actions = [
   {
     name: 'create-task',
-    icon: <Icon name="layers" type="material" color={palette.primary} size={32} />,
+    icon: <Icon name="layers" type="material" color={palette.primary} size={22} />,
     text: i18n.t('updatePlan:addTask'),
     position: 1,
     color: palette.background,
-    textBackground: palette.primary,
+    textBackground: palette.primaryVariant,
+    textStyle: { fontSize: 12, fontFamily: fonts.sansSerif.medium },
+    textElevation: 0,
     textColor: palette.textWhite,
   },
   {
     name: 'create-interaction',
-    icon: <Icon name="group" type="material" color={palette.secondary} size={32} />,
+    icon: <Icon name="group" type="material" color={palette.secondary} size={22} />,
     text: i18n.t('updatePlan:addInteraction'),
     position: 2,
     color: palette.background,
-    textBackground: palette.primary,
+    textBackground: palette.primaryVariant,
+    textStyle: { fontSize: 12, fontFamily: fonts.sansSerif.medium },
+    textElevation: 0,
     textColor: palette.textWhite,
   },
   {
     name: 'create-break',
-    icon: <Icon name="notifications" type="material" color={palette.break} size={32} />,
+    icon: <Icon name="notifications" type="material" color={palette.break} size={22} />,
     text: i18n.t('updatePlan:addBreak'),
     position: 3,
     color: palette.background,
-    textBackground: palette.primary,
+    textBackground: palette.primaryVariant,
+    textStyle: { fontSize: 12, fontFamily: fonts.sansSerif.medium },
+    textElevation: 0,
     textColor: palette.textWhite,
   },
 ];
 
 interface State {
   actionName: string;
+  isOpen: boolean;
 }
 
 export class FixedCreatePlanSubItemButton extends React.PureComponent<{}, State> {
   state: State = {
     actionName: '',
+    isOpen: false,
   };
 
-  onPressItem = (name: any) => {
+  onPressItem = (name?: string) => {
+    const actionName = name || 'create-task';
     this.setState({
-      actionName: name,
+      actionName,
     });
+  };
+
+  onOpen = () => {
+    this.setState({ isOpen: true });
+  };
+
+  onClose = () => {
+    this.setState({ isOpen: false });
   };
 
   render() {
     return (
       <FloatingAction
-        color={palette.secondary}
+        color={this.state.isOpen ? palette.secondary : palette.primaryVariant}
         actions={actions}
-        overlayColor="rgba(97, 91, 185, 1)"
+        overlayColor={palette.modalBackgroundOverlay}
         onPressItem={this.onPressItem}
+        floatingIcon={
+          this.state.isOpen ? (
+            <Icon name="close" type="material" color={palette.textWhite} size={32} />
+          ) : (
+            <Icon name="add" type="material" color={palette.secondary} size={32} />
+          )
+        }
+        onOpen={this.onOpen}
+        onClose={this.onClose}
       />
     );
   }
