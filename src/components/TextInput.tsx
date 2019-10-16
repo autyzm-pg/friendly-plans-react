1c892d8a965ef02215c406adf5978de0c7f69cc8
maciejhein <45594475+maciejhein@users.noreply.github.com>
2019-10-16 11:05:15 +0200

    #150 Possibility to edit plan (#171)
    * custom input
    
    * add validation to form
    
    * navigate to update plan
    
    * adjust textinput to form
    
    * fix import ordering
    
    * fix gradle version

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,5 +1,5 @@
-import React, { SFC } from 'react';
-import { StyleSheet, TextInput as BaseTextInput, TextInputProps, View } from 'react-native';
+import React from 'react';
+import { KeyboardAvoidingView, StyleSheet, TextInput as BaseTextInput, TextInputProps } from 'react-native';
 
 import { palette, typography } from 'styles';
 
@@ -7,28 +7,62 @@ interface Props extends TextInputProps {
   hideUnderline?: boolean;
 }
 
-export const TextInput: SFC<Props> = ({ hideUnderline, style, ...inputProps }) => (
-  <View style={[styles.inputWrapper, style, hideUnderline ? {} : styles.inputUnderline]}>
-    <BaseTextInput
-      autoCorrect={false}
-      style={[styles.input]}
-      placeholderTextColor={palette.textInputPlaceholder}
-      {...inputProps}
-    />
-  </View>
-);
+interface State {
+  isEditable: boolean;
+}
+
+export class TextInput extends React.PureComponent<Props, State> {
+  state: State = {
+    isEditable: !this.props.value,
+  };
+
+  handleFocus = () => this.setState({ isEditable: true });
+
+  handleBlur = () => {
+    if (this.props.value) {
+      this.setState({ isEditable: false });
+    }
+  };
+
+  render() {
+    const { isEditable } = this.state;
+    const { style, hideUnderline, ...inputProps } = this.props;
+
+    return (
+      <KeyboardAvoidingView
+        style={[
+          styles.inputWrapper,
+          style,
+          !hideUnderline && isEditable && styles.inputUnderline,
+          isEditable && styles.inputBackground,
+        ]}
+      >
+        <BaseTextInput
+          style={styles.input}
+          placeholderTextColor={palette.textInputPlaceholder}
+          autoCorrect={false}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+          {...inputProps}
+        />
+      </KeyboardAvoidingView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   input: {
     height: 28,
     ...typography.subtitle,
-    color: palette.primaryVariant,
-    backgroundColor: palette.backgroundAdditional,
     paddingVertical: 4,
+    color: palette.textBlack,
   },
   inputUnderline: {
     borderBottomColor: palette.primary,
     borderBottomWidth: 1,
+  },
+  inputBackground: {
+    backgroundColor: palette.backgroundAdditional,
   },
   inputWrapper: {
     flex: 1,
