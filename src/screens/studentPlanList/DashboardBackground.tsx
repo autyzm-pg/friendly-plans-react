9ad283dae670f3ffce771a6ca6b62f135997d0cc
Kinga Wigurska-Bąk <kinga.wigurskabak@wipro.com>
2019-10-23 15:46:54 +0200

    Add svg icons, create background

--- src/screens/studentPlanList/DashboardBackground.tsx
+++ src/screens/studentPlanList/DashboardBackground.tsx
@@ -1,27 +1,93 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { SvgXml } from 'react-native-svg';
+import { StyleSheet } from 'react-native';
+
+import {
+  BackgroundEllipse,
+  EllipseBlue,
+  EllipseLight,
+  EllipsePink,
+  EllipsePurple,
+  LeafBlue,
+  LeafLight,
+  LeafNavyBlue,
+  LeafNavyBlueSmall,
+  LeafOrange,
+  LeafPink,
+} from '../../assets/images/icons';
 
 export class DashboardBackground extends React.PureComponent {
   render() {
-    const bgEllipse = `
-      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="721.935" height="721.935" viewBox="0 0 721.935 721.935">
-        <defs>
-            <linearGradient id="a" x1="0.293" y1="0.007" x2="1.013" y2="0.644" gradientUnits="objectBoundingBox">
-                <stop offset="0" stop-color="#e6e8ff"/>
-                <stop offset="1" stop-color="#f0f0fd"/>
-            </linearGradient>
-        </defs>
-        <circle cx="349" cy="349" r="349" transform="matrix(0.999, -0.035, 0.035, 0.999, 0, 24.36)" style="fill:url(#a)"/>
-      </svg>
-  `;
-    return <SvgXml xml={bgEllipse} style={styles.bgEllipse} />;
+    return (
+      <>
+        <LeafLight style={styles.leafLight} />
+        <EllipseLight style={styles.ellipseLight} />
+        <BackgroundEllipse style={styles.bgEllipse} />
+        <EllipsePink style={styles.ellipsePink} />
+        <LeafOrange style={styles.leafOrange} />
+        <LeafNavyBlueSmall style={styles.leafNavyBlueSmall} />
+        <LeafBlue style={styles.leafBlue} />
+        <LeafPink style={styles.leafPink} />
+        <EllipseBlue style={styles.ellipseBlue} />
+        <EllipsePurple style={styles.ellipsePurple} />
+        <LeafNavyBlue style={styles.leafNavyBlue} />
+      </>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   bgEllipse: {
     position: 'absolute',
+    top: '24%',
+  },
+  ellipseBlue: {
+    position: 'absolute',
     top: '30%',
+    right: '32%',
+  },
+  ellipseLight: {
+    position: 'absolute',
+    top: '23%',
+    left: '24%',
+  },
+  ellipsePink: {
+    position: 'absolute',
+    bottom: '20%',
+    left: '13%',
+  },
+  ellipsePurple: {
+    position: 'absolute',
+    bottom: '14%',
+    right: '12%',
+  },
+  leafBlue: {
+    position: 'absolute',
+    top: '12%',
+    left: '8%',
+  },
+  leafLight: {
+    position: 'absolute',
+    bottom: '17%',
+    left: '5%',
+  },
+  leafNavyBlue: {
+    position: 'absolute',
+    top: '25%',
+    right: '9%',
+  },
+  leafNavyBlueSmall: {
+    position: 'absolute',
+    top: '9%',
+    left: '5%',
+  },
+  leafOrange: {
+    position: 'absolute',
+    bottom: '-8%',
+    left: '17%',
+  },
+  leafPink: {
+    position: 'absolute',
+    bottom: '-5%',
+    right: '2%',
   },
 });
