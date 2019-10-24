887d8702e4fd8fc6b08f5001ced56c3a004d7d85
kwbak <kinga.wigurskabak@wipro.com>
2019-10-24 10:42:13 +0200

    Review fixes

--- src/screens/planActivity/TableRow.tsx
+++ src/screens/planActivity/TableRow.tsx
@@ -14,55 +14,46 @@ interface Props {
   planItem: PlanItem;
 }
 
-export class TableRow extends React.PureComponent<Props> {
-  navigateToPlanItemUpdate = (planItem: PlanItem) => {
+export const TableRow: React.FunctionComponent<Props> = ({ rowNumber, planItem, border }) => {
+  const navigateToPlanItemUpdate = () => {
     NavigationService.navigate('PlanItemTask', {
       planItem,
     });
   };
 
-  onUpdate = () => {
-    const { planItem } = this.props;
-
-    this.navigateToPlanItemUpdate(planItem);
+  const onDelete = () => {
+    planItem.delete();
   };
 
-  onDelete = () => {
-    this.props.planItem.delete();
-  };
-
-  render() {
-    const { planItem, border, rowNumber } = this.props;
-    return (
-      <View style={[styles.row, border && styles.rowBorder]}>
-        <View style={[styles.cell, { flex: CELL_DIMENSIONS.NUMBER, alignItems: 'center' }]}>
-          <Text style={styles.text}>{rowNumber}</Text>
-        </View>
-        <View style={[styles.cell, { flex: CELL_DIMENSIONS.CHECKBOX, alignItems: 'center' }]}>
-          <CheckboxInput checked onPress={noop} />
-        </View>
-        <View style={[styles.cell, { flex: CELL_DIMENSIONS.NAME }]}>
-          <Text style={styles.text}>{planItem.name}</Text>
-        </View>
-        <View style={[styles.cell, { flex: CELL_DIMENSIONS.TYPE, alignItems: 'flex-start' }]}>
-          <Icon name={planItem.getIconName()} type="material" />
-        </View>
-        <View style={[styles.cell, { flex: CELL_DIMENSIONS.SECTION }]}>
-          <Text style={styles.text}>5</Text>
-        </View>
-        <View style={[styles.cell, { flex: CELL_DIMENSIONS.TIME }]}>
-          <Text style={styles.text}>{planItem.time}</Text>
-        </View>
-        <View style={[styles.cell, { flex: CELL_DIMENSIONS.DELETE, alignItems: 'flex-start' }]}>
-          <IconButton name="delete" size={24} color={palette.textInputPlaceholder} onPress={this.onDelete} />
-        </View>
-        <View style={[styles.cell, { flex: CELL_DIMENSIONS.EDIT, alignItems: 'center' }]}>
-          <IconButton name="pencil" size={24} onPress={this.onUpdate} />
-        </View>
+  return (
+    <View style={[styles.row, border && styles.rowBorder]}>
+      <View style={[styles.cell, { flex: CELL_DIMENSIONS.NUMBER, alignItems: 'center' }]}>
+        <Text style={styles.text}>{rowNumber}</Text>
       </View>
-    );
-  }
-}
+      <View style={[styles.cell, { flex: CELL_DIMENSIONS.CHECKBOX, alignItems: 'center' }]}>
+        <CheckboxInput checked onPress={noop} />
+      </View>
+      <View style={[styles.cell, { flex: CELL_DIMENSIONS.NAME }]}>
+        <Text style={styles.text}>{planItem.name}</Text>
+      </View>
+      <View style={[styles.cell, { flex: CELL_DIMENSIONS.TYPE, alignItems: 'flex-start' }]}>
+        <Icon name={planItem.getIconName()} type="material" />
+      </View>
+      <View style={[styles.cell, { flex: CELL_DIMENSIONS.SECTION }]}>
+        <Text style={styles.text}>5</Text>
+      </View>
+      <View style={[styles.cell, { flex: CELL_DIMENSIONS.TIME }]}>
+        <Text style={styles.text}>{planItem.time}</Text>
+      </View>
+      <View style={[styles.cell, { flex: CELL_DIMENSIONS.DELETE, alignItems: 'flex-start' }]}>
+        <IconButton name="delete" size={24} color={palette.textInputPlaceholder} onPress={onDelete} />
+      </View>
+      <View style={[styles.cell, { flex: CELL_DIMENSIONS.EDIT, alignItems: 'center' }]}>
+        <IconButton name="pencil" size={24} onPress={navigateToPlanItemUpdate} />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   row: {
